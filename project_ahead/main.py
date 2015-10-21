5f0e49d4771e0f332e9803d615c9c1fb40ec9040
GreeeeeShot <joonbum638@naver.com>
2015-10-21 23:14:52 +0900

    적 오브젝트 구현 및 hp 감소 구현
    적 오브젝트 이동 ai 와 z키에 의한 적 오브젝트 hp 감소, 적공격에 의한 플레이어 오브젝트의 hp 감소 구현

--- project_ahead/main.py
+++ project_ahead/main.py
@@ -16,6 +16,38 @@ class Grass:
         elif self.x <= -400:
             self.x = 400
 
+class Enemy:
+    def __init__(self):
+        global stage
+        self.x, self.y = 900, 100
+        self.frame = 0
+        self.image = load_image('animation_sheet.png')
+        self.hp = 100 * stage
+        self.atk = 5 * stage
+        self.attack_time = 0
+    def update(self):
+        global enemy_count, go,attack,object,team
+        self.frame = (self.frame +1) % 8
+        self.attack_time += 1
+        if self.hp <= 0:
+            self.x = 800
+            self.hp = 100
+        if self.x > 250:
+            self.x -= 10
+        else:
+            go = False
+            for object in team:
+                if attack and object.num == 2:
+                    self.hp -= object.atk
+                    attack = False
+                if self.attack_time >= 30 and object.num == 2:
+                    object.hp -= self.atk
+                    self.attack_time = 0
+        if self.x == 300:
+            enemy_count += 1
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+
 class Object:
     def __init__(self):
         self.go = random.randint(1,2)
@@ -23,13 +55,20 @@ class Object:
         self.frame = 0
         self.num = None
         self.image = load_image('run_animation.png')
+        self.hp = 100
+        self.atk = 10
     def update(self):
+        if self.hp == 0:
+            self.y += 100
+            self.hp = 100
         self.frame = (self.frame +1) % 8
+        if self.num == 0:
+            self.atk = 20
     def draw(self):
         self.image.clip_draw(self.frame*100,0,100,100,self.x+50*self.num,self.y)
 
 def handle_events():
-    global running, go
+    global running, go, attack
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -40,19 +79,27 @@ def handle_events():
             go = True
         elif event.type == SDL_KEYUP and event.key == SDLK_RIGHT:
             go = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
+            attack = True
 
+attack = False
 num=0
+stage=1
+enemy_count=1
 team = None
 go = None
 object = None
+enemy = None
+enemy_army = None
 grass1 = None
 grass2 = None
 running = True
 
 def enter():
-    global grass1,grass2, team,num,go
+    global grass1,grass2, team,num,go,enemy,stage,enemy_army
     open_canvas()
     team = [Object() for i in range(3)]
+    enemy_army = [Enemy() for j in range(100)]
     go = False
     grass1 = Grass()
     grass2 = Grass()
@@ -63,18 +110,29 @@ def enter():
         num += 1
 
 def exit():
-    global boy, grass1,grass2, team
+    global boy, grass1,grass2, team,object,enemy,enemy_army,stage,enemy_count
     del(object)
+    del(enemy_count)
     del(grass1)
     del(grass2)
     del(team)
+    del(enemy)
+    del(stage)
+    del(enemy_army)
     close_canvas()
 
 def update():
+    army = 0
     grass1.update()
     grass2.update()
     for object in team:
         object.update()
+    for enemy in enemy_army:
+        enemy.update()
+        if army >= enemy_count:
+            break
+        else:
+            army += 1
 
 def draw():
     clear_canvas()
@@ -82,6 +140,8 @@ def draw():
     grass2.draw()
     for object in team:
         object.draw()
+    for enemy in enemy_army:
+        enemy.draw()
     update_canvas()
 
 def main():
