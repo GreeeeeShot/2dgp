f5348df53a1ee77bdacbc80a0bc83eb9c4378c15
GreeeeeShot <joonbum638@naver.com>
2015-10-25 21:14:20 +0900

    사기 시스템 내부 프로그래밍 구현
    사기 시스템 내부 시스템 구현

--- project_ahead/main.py
+++ project_ahead/main.py
@@ -1,6 +1,19 @@
 import random
 from pico2d import *
 
+class Pever_Time:
+    def __init__(self):
+        self.success_image = load_image('pever.png')
+        self.fail_image = load_image('fail.png')
+        self.x = 400
+        self.y = 300
+
+    def draw(self):
+        if pever == 1:
+            self.success_image.draw(self.x,self.y)
+        elif pever == 2:
+            self.fail_image.draw(self.x,self.y)
+
 class Map:
     def __init__(self):
         self.image = load_image('map.png')
@@ -11,8 +24,13 @@ class Map:
         self.moving_y = 480
 
     def update(self):
+        global pever,running
         if go:
             self.moving_x+=2
+        if self.moving_x >= 500:
+            pever = 1
+        if self.moving_x >= 670:
+            running = False
     def draw(self):
         self.image.draw(self.x,self.y)
         self.moving_image.draw(self.moving_x,self.moving_y)
@@ -24,7 +42,7 @@ class Enemy_Arrow:
         self.frame = 0
         self.image = load_image('animation_sheet.png')
         self.arrow_image = load_image('arrow.png')
-        self.hp = 100 * stage
+        self.hp = 50 * stage
         self.atk = 100 * stage
         self.attack_time = 0
         self.arrow_x = self.x
@@ -53,13 +71,13 @@ class Enemy_Arrow:
             self.arrow_x = self.x
             self.arrow_y = self.y
         if self.arrow_up:
-            self.arrow_y +=5
+            self.arrow_y +=8
         else:
-            self.arrow_y -= 10
+            self.arrow_y -= 8
         if self.arrow_up and self.arrow_y >= 200:
             self.arrow_up = False
         for object in team:
-            if object.x+20+(object.num*25)>=self.arrow_x-40 and object.x-20+(object.num*25)<= self.arrow_x-40 and self.arrow_y <=130:
+            if object.x+20+(object.num*20)>=self.arrow_x-40 and object.x-20+(object.num*20)<= self.arrow_x-40 and self.arrow_y <=110:
                 object.hp -= self.atk
                 self.arrow_attack = False
                 self.arrow_x = 900
@@ -73,9 +91,12 @@ class Enemy_Arrow:
             self.arrow_up = True
 
     def update(self):
-        global enemy_count, go,attack,object,team
+        global enemy_count, go,attack,object,team,pever
         self.frame = (self.frame +1) % 8
         self.attack_time += 1
+        if pever==1 and self.atk != 10:
+            self.atk = 10
+            self.hp = 50
         if go:
                 self.x -= 10
                 if self.arrow_attack == True:
@@ -137,47 +158,80 @@ class Enemy:
         self.atk = 100 * stage
         self.attack_time = 0
     def update(self):
-        global enemy_count, go,attack,object,team
+        global enemy_count, go,attack,object,team,pever
         self.frame = (self.frame +1) % 8
         self.attack_time += 1
+        if pever==1 and self.atk != 10:
+            self.atk = 10
+            self.hp = 50
+        if go:
+                self.x -= 10
         if self.hp <= 0:
             self.x = 800
             self.hp = 100
             self.attack_time = 0
+        if self.x == 300:
+            enemy_count+=1
         if self.x > 250:
             self.x -= 10
         else:
-            go = False
-            for object in team:
-                if object.num == 2:
-                    if attack:
-                        self.hp -= object.atk
-                        attack = False
-                    if self.attack_time >= 10:
-                        object.hp -= self.atk
-                        self.attack_time = 0
-        if self.x == 300:
-            enemy_count += 1
+            self.attack_time += 1
+            if self.attack_time >= 50:
+                self.attack_time = 0
+            if self.x<=250:
+                go = False
+                for object in team:
+                    if object.num == 2:
+                        if attack:
+                            self.hp -= object.atk
+                            attack = False
+                        if self.attack_time >= 30:
+                            object.hp -= self.atk
+                            self.attack_time = 0
     def draw(self):
         self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
 
+
 class Object:
     def __init__(self):
-        self.x, self.y = 100, 100
+        self.x, self.y = 100, 70
         self.frame = 0
         self.num = None
-        self.image = load_image('run_animation.png')
+        self.image = load_image('player_character.png')
+        self.attack_image = load_image('player_attack_animation.png')
+        self.attack_frame = 0
+        self.player_attack = False
         self.hp = 100
         self.atk = 10
     def update(self):
-        if self.hp == 0:
+        global pever
+        if self.player_attack:
+            self.attack_animation()
+        if pever==1 and self.atk != 50:
+            self.atk = 50
+            self.hp = 200
+            self.y=70
+        if attack:
+            self.frame = 0
+            self.attack_frame = 0
+            self.player_attack = True
+        if self.hp <= 0:
             self.y += 100
             self.hp = 100
-        self.frame = (self.frame +1) % 8
-        if self.num == 2:
+        if go:
+            self.frame = (self.frame +1) % 6
+        if self.num == 2 and pever == 0:
             self.atk = 20
+    def attack_animation(self):
+        self.attack_frame += 1
+        if self.attack_frame >= 5:
+            self.player_attack = False
+            self.attack_frame = 0
     def draw(self):
-        self.image.clip_draw(self.frame*100,0,100,100,self.x+50*self.num,self.y)
+        if self.player_attack:
+            self.attack_image.clip_draw(self.attack_frame*80,0,80,45,self.x+50*self.num,self.y)
+        else:
+            self.image.clip_draw(self.frame*40,0,40,46,self.x+50*self.num,self.y)
 
 def handle_events():
     global running, go, attack
@@ -193,12 +247,15 @@ def handle_events():
             go = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
             attack = True
+        elif event.type == SDL_KEYUP and event.key == SDLK_z:
+            attack = False
 
 attack = False
 num=0
 stage=1
 enemy_count=1
 team = None
+game_time = None
 go = None
 object = None
 enemy = None
@@ -207,15 +264,18 @@ map=None
 enemy_arrow = None
 grass1 = None
 grass2 = None
+pever_time = None
 pever = None
 running = True
 
 def enter():
-    global map, grass1,grass2, team,num,go,enemy,stage,enemy_army,enemy_arrow
+    global map, grass1,grass2, team,num,go,enemy,stage,enemy_army,enemy_arrow,pever_time,pever, game_time
     open_canvas()
     team = [Object() for i in range(3)]
-    enemy_army = [Enemy() for j in range(10)]
+    enemy_army = [Enemy() for j in range(3)]
     go = False
+    game_time = 100
+    pever_time = Pever_Time()
     grass1 = Grass()
     grass2 = Grass()
     enemy_arrow = Enemy_Arrow()
@@ -228,7 +288,7 @@ def enter():
     map = Map()
 
 def exit():
-    global map,boy, grass1,grass2, team,object,enemy,enemy_army,stage,enemy_count,enemy_arrow
+    global map,boy, grass1,grass2, team,object,enemy,enemy_army,stage,enemy_count,enemy_arrow,pever_time
     del(object)
     del(enemy_count)
     del(grass1)
@@ -237,6 +297,7 @@ def exit():
     del(enemy)
     del(map)
     del(stage)
+    del(pever_time)
     del(enemy_army)
     del(enemy_arrow)
     close_canvas()
@@ -267,6 +328,8 @@ def draw():
     for enemy in enemy_army:
         enemy.draw()
     map.draw()
+    pever_time.draw()
+
     update_canvas()
 
 
